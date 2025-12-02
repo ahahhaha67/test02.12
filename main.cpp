94ca1b88c9076883d6d84b47c9703dac165cc337
ahahhaha67 <krivoshapovewan@yandex.ru>
2025-12-02 09:57:30 +0300

    DOT REALIZATION

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept> 
 
 namespace top {
     struct p_t {
@@ -13,9 +14,15 @@ namespace top {
     bool operator!=(p_t a, p_t b);
 
     struct IDraw {
+        virtual ~IDraw() = default;
         virtual p_t begin() const = 0;
         virtual p_t next(p_t prev) const = 0;
-        virtual ~IDraw() = default;
+    };
+
+    struct Dot: IDraw {
+        p_t begin () const override;
+        p_t next (p_t prev) const override;
+        p_t d;
     };
 } 
 
@@ -23,6 +30,17 @@ int main() {
     using namespace top;
     p_t a{1, 1}, b{1, 1};
     std::cout << (a == b) << "\n";
+}
+
+top::p_t top::Dot::begin() const {
+    return d;
+}
+
+top::p_t top::Dot::next(p_t prev) const {
+    if (prev != d){
+        throw std::logic_error("bad prev");
+    }
+    return d;
 }
 
 bool top::operator==(p_t a, p_t b) {
